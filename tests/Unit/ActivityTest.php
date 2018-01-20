53ca4d7a9357e3956ba23a877d3b97d897f3c741
Jacob Biros <jake@Jacobs-MacBook-Pro.local>
2018-01-20 18:11:08 +0900

    Update Activity stuff

--- tests/Unit/ActivityTest.php
+++ tests/Unit/ActivityTest.php
@@ -6,6 +6,7 @@ namespace Tests\Feature;
 use App\Thread;
 use App\Activity;
 use Tests\TestCase;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class Test extends TestCase
@@ -39,6 +40,21 @@ class Test extends TestCase
         $reply = create('App\Reply');
 
         $this->assertEquals(2, Activity::count());
+    }
+
+    /** @test */
+    function it_fetches_an_activity_feed_for_any_user() {
+        // Given we have a thread
+        $this->signIn();
+        create('App\Thread', [ 'user_id' => auth()->id() ]);
+        // and another thread from a week ago
+        create('App\Thread', [ 'user_id' => auth()->id(), 'created_at' => Carbon::now()->subWeek() ]);
+        // When we fetch their feed
+        $feed = Activity::feed(auth()->user());
+        // Then it should be returned in the proper format.
+        $this->assertTrue($feed->keys()->contains(
+            Carbon::now()->format('Y-m-d')
+        ));
     }
 
 }
